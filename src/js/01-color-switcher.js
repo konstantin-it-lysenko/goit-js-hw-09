39f4bdfba50ecfeef81a1a458d0ed358981a7183
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-25 21:39:40 +0300

    complete tast01.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', startBtnHandler);
+stopBtn.addEventListener('click', stopBtnHandler);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function startBtnHandler() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    timerId = setInterval(() => {
+        let color = getRandomHexColor();
+        console.log(color);
+        document.body.style.backgroundColor = color;
+    }, 1000);
+}
+
+function stopBtnHandler() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(timerId);
+}
 
