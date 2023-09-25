6d1e2d667bb8dc464db8a10630091d2472e6db22
konstantin-it-lysenko <78411901+konstantin-it-lysenko@users.noreply.github.com>
2023-09-25 18:40:01 +0000

    Deploying to gh-pages from @ konstantin-it-lysenko/goit-js-hw-09@39f4bdfba50ecfeef81a1a458d0ed358981a7183 🚀

---
+++ 01-color-switcher.16183a4f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,o=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;console.log(t),document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.16183a4f.js.map
